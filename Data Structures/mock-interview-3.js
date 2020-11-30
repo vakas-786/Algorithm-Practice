f6ae84b8b0bb3115411d61688837de934dd84f11
vakas-786 <vakas0959@gmail.com>
2020-11-30 01:10:21 -0500

    solution

--- Data Structures/mock-interview-3.js
+++ Data Structures/mock-interview-3.js
@@ -1,19 +1,22 @@
 // write a function that lets you watch 2 movies conscecutively. 
-// Array of movie times. Return if its possible to watch 2 movies conscecutively. Movie times need to be exact 
-//Objects 
+// Array of movie times. Return if its possible to watch 2 movies conscecutively. Total of movies watched need to be exact length of flight
 
-const movie_times = [80, 100, 80, 1, 75]
+const movie_times = [80, 100, 80]
 // [80, 100, 80]
 const flight = 160 
 
 function is_possible(movie_times, flight){
-
-    const movieObject = {}
+    const movie_seen = new Set()
     for(let i = 0; i < (movie_times.length); i ++){
-        const currentMovie =  movieArray[i]
-        if(movieObject.has(currentMovie) {
-            
+        const first = movie_times[i]
+        const secondMovie = flight - first 
+
+        if(movie_seen.has(secondMovie)) {
+            return console.log('possible') 
         } 
+        movie_seen.add(first)
     }
+    return console.log('impossible')
 }
 
+is_possible(movie_times, flight)
