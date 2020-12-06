8bf74ff39a6b7a84fdb33f7d2217af63d823f8fa
vakas-786 <vakas0959@gmail.com>
2020-12-05 22:59:48 -0500

    solution

---
+++ Data Structures/roman-numerals.js
@@ -0,0 +1,15 @@
+var romanToInt = function(s) {
+    const map = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000 }
+    let num = 0
+    for (let i = 0; i < s.length; i ++ ){
+       if ( map[s[i]] < map[s[i+1]]){
+           num -= map[s[i]]
+       }else {
+           num += map[s[i]]
+       } 
+    }
+    
+    return num
+};
+const s = "III"
+console.log(romanToInt(s))
