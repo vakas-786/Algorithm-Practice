c56b3ef00f87fdd8c1ccc831b1ba86dec6e0638d
vakas-786 <vakas0959@gmail.com>
2020-12-11 21:57:57 -0500

    solution

---
+++ Data Structures/longest-palindrome.js
@@ -0,0 +1,23 @@
+var longestPalindrome = function(s) {
+    if (s.length < 1) return "";
+  let maxSubStart = 0;
+  let maxSubLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    const lengthCenteredAtChar = expandAroundCenter(s, i, i);
+    const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+    const longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace)
+    if (longestSubAtChar > maxSubLength) {
+      maxSubLength = longestSubAtChar;
+      maxSubStart = i - Math.floor((maxSubLength - 1) / 2);
+    }
+  }
+  return s.substr(maxSubStart, maxSubLength);
+}
+
+function expandAroundCenter(s, left, right) {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
+  }
+  return right - left - 1;
+};
