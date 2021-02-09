e2a07402b31e607747b16cfaa769964091a5edb5
vakas-786 <vakas0959@gmail.com>
2021-02-08 22:30:33 -0500

    typo

--- Data Structures/plusOne.js
+++ Data Structures/plusOne.js
@@ -1,5 +1,5 @@
 var plusOne = function(digits) {
-    for (i = digits.length -1; i >= 0; i--){
+    for (let i = digits.length -1; i >= 0; i--){
         digits[i] += 1
         if (digits[i] > 9){
             digits[i] = 0
